891eb67027560008d0e09f22b2ca6731ed6e5ad3
abdulrazzak <abdulrazzaksabbagh10@gmail.come>
2024-08-08 22:23:34 +0300

    creting the circels

--- lib/screens/signingpage.dart
+++ lib/screens/signingpage.dart
@@ -1,3 +1,4 @@
+import 'package:collegerates/constants.dart';
 import 'package:flutter/material.dart';
 
 class Signingpage extends StatelessWidget {
@@ -5,6 +6,52 @@ class Signingpage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Constants.c_white,
+      body: Stack(
+        children: [
+          Positioned(
+            top: screenHeight * 0.1,
+            left: -screenWidth * 0.2,
+            child: Container(
+              width: screenWidth * 0.4,
+              height: screenHeight * 0.3,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Constants.c_main.withOpacity(0.85),
+              ),
+            ),
+          ),
+          Positioned(
+            top: screenHeight * -0.1,
+            right: -screenWidth * 0.12,
+            child: Container(
+              width: screenWidth * 0.45,
+              height: screenHeight * 0.32,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Constants.c_secondery.withOpacity(0.85),
+              ),
+            ),
+          ),
+          Positioned(
+            top: screenHeight * 0.4,
+            right: -screenWidth * 0.075,
+            child: Container(
+              width: screenWidth * 0.22,
+              height: screenHeight * 0.3,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Constants.c_main.withOpacity(0.85),
+              ),
+            ),
+          )
+        
+          
+        ],
+      ),
+    );
   }
 }
